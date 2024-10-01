9b3498a61b3f1a0189ed0c089aeb44f807dcbd68
ThiagoMurbach <thiago.murbach.soares@escola.pr.gov.br>
2024-10-01 07:52:49 -0300

    Add files via upload

---
+++ Trabalho-dos-Cart-o-main/Trabalho-dos-Cart-o-main/Pagina_web_desenvolvendo_uma_ferramenta_interativa_de_estudo-main/Página web desenvolvendo uma ferramenta interativa de estudo/perguntas.js
@@ -0,0 +1,61 @@
+criaCartao( //cria o cartao 
+    'Cibercrimes', //tópico da pergunta
+    'O que são cibercrimes?', //pergunta
+    'Crime que acontece no ambiente virtual, para cometer fraudes e invadir sistemas para obter informações pessoais ou financeiras.' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Cibercrimes', //tópico da pergunta
+    'Cite exemplos de cibercrimes?', //pergunta
+    'Phising, fraudes bancarias, clonagem de cartão' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Cibercrimes', //tópico da pergunta
+    'O que é a LGPD?', //pergunta
+    'Lei brasileira aprovada em 2018 que controla a privacidade e o uso/tratamento de dados pessoais.' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Cibercrimes', //tópico da pergunta
+    'Qual e o objetivo da LGPD?', //pergunta
+    'controla a privacidade e o uso/tratamento de dados pessoais' //resposta
+)
+criaCartao( //cria o cartao 
+    'Internet', //tópico da pergunta
+    'Quando a internet foi criada?', //pergunta
+    '29 de outubro de 1969' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Internet', //tópico da pergunta
+    'O que são cabos de transmissão intercontinentais de internet?Qual e a velocidade de transmissão?', //pergunta
+    'Conexões submersas nos oceanos usadas para transmitir sinais de telecomunicações.' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Internet', //tópico da pergunta
+    'O que são roteadores?', //pergunta
+    'Uma função é um bloco de código que executa alguma tarefa' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Internet', //tópico da pergunta
+    'O que sao servidores?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
+criaCartao( //cria o cartao 
+    'Inteligẽncia Artificial', //tópico da pergunta
+    'O que e Inteligência Artificial?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
+criaCartao( //cria o cartao 
+    'Inteligẽncia Artificial', //tópico da pergunta
+    'Qual foi a primeira IA do mundo?Por quem foi criada?Qual era seu objetivo?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
+criaCartao( //cria o cartao 
+    'Inteligẽncia Artificial', //tópico da pergunta
+    'Qual o impacto da Inteligência Artificial no mundo?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
