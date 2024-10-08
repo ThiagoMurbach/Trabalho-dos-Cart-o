9bdf31367cd35ab9dad7eb80927b82bc53108562
ThiagoMurbach <thiago.murbach.soares@escola.pr.gov.br>
2024-10-08 07:41:29 -0300

    Add files via upload

---
+++ Página web desenvolvendo uma ferramenta interativa de estudo/perguntas.js
@@ -0,0 +1,61 @@
+criaCartao( //cria o cartao 
+    'Cibercrimes', //tópico da pergunta
+    'O que são cibercrimes?', //pergunta
+    'Crime que acontece no ambiente virtual, para cometer fraudes e invadir sistemas para obter informações pessoais ou financeiras.' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Cibercrimes', //tópico da pergunta
+    'Cite exemplos de cibercrimes?', //pergunta
+    'Phising, fraudes bancarias, clonagem de cartão' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Cibercrimes', //tópico da pergunta
+    'O que é a LGPD?', //pergunta
+    'Lei brasileira aprovada em 2018 que controla a privacidade e o uso/tratamento de dados pessoais.' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Cibercrimes', //tópico da pergunta
+    'Qual e o objetivo da LGPD?', //pergunta
+    'controla a privacidade e o uso/tratamento de dados pessoais' //resposta
+)
+criaCartao( //cria o cartao 
+    'Internet', //tópico da pergunta
+    'Quando a internet foi criada?', //pergunta
+    '29 de outubro de 1969' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Internet', //tópico da pergunta
+    'O que são cabos de transmissão intercontinentais de internet?Qual e a velocidade de transmissão?', //pergunta
+    'Conexões submersas nos oceanos usadas para transmitir sinais de telecomunicações.Sua velocidade e 20Mb/s' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Internet', //tópico da pergunta
+    'O que são roteadores?', //pergunta
+    'É um dispositivo que encaminha pacotes de dados entre redes de computadores, criando um conjunto de redes de sobreposição' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Internet', //tópico da pergunta
+    'O que sao servidores?', //pergunta
+    'É um software ou computador, com sistema de computação centralizada que fornece serviços a uma rede de computadores.' //resposta
+)
+criaCartao( //cria o cartao 
+    'Inteligẽncia Artificial', //tópico da pergunta
+    'O que e Inteligência Artificial?', //pergunta
+    'É um campo de estudo multidisciplinar que abrange varias áreas do conhecimento.' //resposta
+)
+criaCartao( //cria o cartao 
+    'Inteligẽncia Artificial', //tópico da pergunta
+    'Qual foi a primeira IA do mundo?Por quem foi criada?Qual era seu objetivo?', //pergunta
+    'A Máquina de Turingi.Warren McCulloch e Walter Pitts.Criar máquinas que pudessem exibir inteligência semelhante à humana.' //resposta
+)
+criaCartao( //cria o cartao 
+    'Inteligẽncia Artificial', //tópico da pergunta
+    'Qual o impacto da Inteligência Artificial no mundo?', //pergunta
+    'Surgem preocupações sobre questões éticas, como justiça, liberdade, privacidade, segurança e responsabilidade.' //resposta
+)
